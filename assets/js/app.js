2199b27ae99bc0e6b9c520ae66842434358bc3ea
meleryz <meleryz@gmail.com>
2020-07-15 22:48:07 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,195 @@
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import "../css/app.scss"
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
+
+// webpack automatically bundles all modules in your
+// entry points. Those entry points can be configured
+// in "webpack.config.js".
+//
+// Import deps with the dep name or local files with a relative path, for example:
+//
+//     import {Socket} from "phoenix"
+//     import socket from "./socket"
+//
+import "phoenix_html"
+import {Socket} from "phoenix"
+import NProgress from "nprogress"
+import {LiveSocket} from "phoenix_live_view"
+import regeneratorRuntime from "regenerator-runtime"
+import adapter from "webrtc-adapter"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
+var users = {}
+var localStream
+
+async function initStream() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true, width: "1280"})
+    localStream = stream;
+    document.getElementById("local-video").srcObject = stream
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function addUserConnection(userUuid) {
+  if (users[userUuid] === undefined) {
+    users[userUuid] = {
+      peerConnection: null
+    }
+  }
+
+  return users;
+}
+
+function removeUserConnection(userUuid) {
+  let pc = users[userUuid].peerConnection
+  if (pc !== null) {
+    pc.close()
+  }
+
+  delete users[userUuid]
+
+  return users
+}
+
+function createPeerConnection(lv, fromUser, offer) {
+  let newPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      { urls: "stun:littlechat.app:3478" }
+    ]
+  })
+  users[fromUser].peerConnection = newPeerConnection;
+
+  // Add each local track to the RTCPeerConnection.
+  localStream.getTracks().forEach(track => newPeerConnection.addTrack(track, localStream))
+
+  // If creating an answer, rather than an initial offer.
+  if (offer !== undefined) {
+    newPeerConnection.setRemoteDescription({type: "offer", sdp: offer})
+    newPeerConnection.createAnswer()
+      .then((answer) => {
+        newPeerConnection.setLocalDescription(answer)
+        console.log("Sending this ANSWER to the requester:", answer)
+        lv.pushEvent("new_answer", {toUser: fromUser, description: answer})
+      })
+      .catch((err) => console.log(err))
+  }
+
+  newPeerConnection.onicecandidate = async ({candidate}) => {
+    // fromUser is the new value for toUser because we're sending this data back
+    // to the sender
+    lv.pushEvent("new_ice_candidate", {toUser: fromUser, candidate})
+  }
+
+  // Don't add the `onnegotiationneeded` callback when creating an answer due to
+  // a bug in Chrome.
+  if (offer === undefined) {
+    newPeerConnection.onnegotiationneeded = async () => {
+      try {
+        newPeerConnection.createOffer()
+          .then((offer) => {
+            newPeerConnection.setLocalDescription(offer)
+            console.log("Sending this OFFER to the requester:", offer)
+            lv.pushEvent("new_sdp_offer", {toUser: fromUser, description: offer})
+          })
+          .catch((err) => console.log(err))
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  newPeerConnection.ontrack = async (event) => {
+    console.log("Track received:", event)
+    document.getElementById(`video-remote-${fromUser}`).srcObject = event.streams[0]
+  }
+
+  return newPeerConnection;
+}
+
+let Hooks = {}
+Hooks.JoinCall = {
+  mounted () {
+    initStream()
+  }
+}
+
+Hooks.HandleOfferRequest = {
+  mounted () {
+    console.log("new offer request from", this.el.dataset.fromUserUuid)
+    let fromUser = this.el.dataset.fromUserUuid
+
+    createPeerConnection(this, fromUser)
+  }
+}
+
+Hooks.HandleIceCandidateOffer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let iceCandidate = JSON.parse(data.iceCandidate)
+    let peerConnection = users[fromUser].peerConnection
+
+    console.log("new ice candidate from", fromUser, iceCandidate)
+
+    peerConnection.addIceCandidate(iceCandidate)
+  }
+}
+
+Hooks.HandleSdpOffer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+
+    if (sdp != "") {
+      console.log("new sdp OFFER from", data.fromUserUuid, data.sdp)
+
+      createPeerConnection(this, fromUser, sdp)
+    }
+  }
+}
+
+Hooks.HandleAnswer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+    let peerConnection = users[fromUser].peerConnection
+
+    if (sdp != "") {
+      console.log("new sdp ANSWER from", fromUser, sdp)
+      peerConnection.setRemoteDescription({type: "answer", sdp: sdp})
+    }
+  }
+}
+
+Hooks.InitUser = {
+  mounted () {
+    addUserConnection(this.el.dataset.userUuid)
+  },
+
+  destroyed () {
+    removeUserConnection(this.el.dataset.userUuid)
+  }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
+
+// Show progress bar on live navigation and form submits
+window.addEventListener("phx:page-loading-start", info => NProgress.start())
+window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+
+// connect if there are any LiveViews on the page
+liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)
+window.liveSocket = liveSocket
